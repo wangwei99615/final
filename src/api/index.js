13735e2e5cf080ee86261c037e81ec3acfa63967
gouzi <2235222898@qq.com>
2020-09-03 14:18:32 +0800

    axios配置完毕

---
+++ src/api/index.js
@@ -0,0 +1,9 @@
+// 导入axios
+import axios from 'axios'
+
+// 配置的axios
+// baseURL 配置基准地址
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0/'
+
+// 导出配置好的axios（挂载到main）
+export default axios
