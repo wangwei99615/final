3fbfc9295a19e1ecff94aa018bd38646776aea92
gouzi <2235222898@qq.com>
2020-10-03 15:36:56 +0800

    主页布局初始化

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,6 +1,12 @@
 <template>
   <div class='container'>
-    home
+    <el-container class="home-container">
+      <el-aside width="200px">Aside</el-aside>
+      <el-container>
+        <el-header>Header</el-header>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
@@ -10,4 +16,17 @@ export default {
 </script>
 
 <style scoped lang="less">
+.home-container {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  .el-aside {
+    background-color: #002033;
+  }
+  .el-header {
+    border-bottom: 1px solid #ddd;
+  }
+}
 </style>
